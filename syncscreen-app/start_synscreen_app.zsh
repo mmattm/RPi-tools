5f58ede274350b76c789dd4d25581c24c45de0b1
Matthieu Minguet <minguet.mat@gmail.com>
2024-07-05 13:45:48 +0200

    push

--- syncscreen-app/start_synscreen_app.zsh
+++ syncscreen-app/start_synscreen_app.zsh
@@ -41,31 +41,4 @@ sleep 2
 /opt/homebrew/bin/node /Users/expo/Desktop/RPi-tools/syncscreen-app/backend/index.js &
 #/opt/homebrew/bin/node /Users/matthieu.minguet/Code/ECAL/Workshops/JOVaud/syncscreen-app/backend/index.js &
 
-sleep 2
 
-# Reopen Safari to the specific URL and refresh the page
-osascript <<EOF
-tell application "Safari"
-    activate
-    open location "http://localhost:3000"
-    delay 2
-    reload document 1
-    do JavaScript "
-        document.body.style.overflow = 'auto';  // Ensure the body is scrollable
-        document.documentElement.style.overflow = 'auto';  // Ensure the HTML element is scrollable
-        if (document.body.requestFullscreen) {
-            document.body.requestFullscreen();
-        } else if (document.body.webkitRequestFullscreen) { // Safari
-            document.body.webkitRequestFullscreen();
-        } else if (document.body.mozRequestFullScreen) { // Firefox
-            document.body.mozRequestFullScreen();
-        } else if (document.body.msRequestFullscreen) { // IE/Edge
-            document.body.msRequestFullscreen();
-        }
-        // Add event listener for scrolling
-        window.addEventListener('scroll', function() {
-            console.log('scrolling');
-        });
-    " in document 1
-end tell
-EOF
